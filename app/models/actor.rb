284da380ec179511c0a1ac1a207f02b322d6d9d7
Ming Duan <11203duan@gmail.com>
2019-05-13 20:56:01 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,27 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+def full_name
+  a = self.first_name
+  b = self.last_name
+  c = []
+  c << a
+  c << b
+  c.join (" ")
+end
+
+def list_roles
+  d = []
+  self.characters.collect do |x|
+    a = x.name
+    b = x.show.name
+    c = []
+    c << a
+    c << b
+    d << c.join(" - ")
+  end
+  d
+end
+
 end
