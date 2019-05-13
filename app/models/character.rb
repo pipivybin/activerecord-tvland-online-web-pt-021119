4f90561e000f12b1e7981f11e650643ebc8bde69
Ming Duan <11203duan@gmail.com>
2019-05-13 21:06:19 +0000

    Done.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,19 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :actor
+  belongs_to :show
+
+  def say_that_thing_you_say
+    a = self.name
+    b = self.catchphrase
+    "#{a} always says: #{b}"
+  end
+
+  def build_show(hash = {})
+    self.show = Show.new(hash)
+  end
+
+  def build_network(hash)
+    self.network = Network.new(hash)
+  end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def build_network(hash)
+    self.network = Network.new(hash)
+  end
+
 end
